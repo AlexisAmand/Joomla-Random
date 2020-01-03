57d2aaf27960c89205d6e263ccb5d2a708b5b663
Alex <alexis.amand@gmail.com>
2020-01-03 11:44:20 +0100

    Optimisation du code avec array_rand()
    J'ai remplacé une grosse partie du code, dont une boucle for, par une fonction PHP que je ne connaissais pas mais dont je me suis soudain douté de l'existence.

--- random.php
+++ random.php
@@ -2,7 +2,7 @@
 
 /* Affichage d'un article aléatoire d'un blog qui tourne avec Joomla 3.X */
 /* Cette version utilise l'API de Joomla */
-/* version 0.2.1 */
+/* version 0.3 */
 
 define('_JEXEC', 1);
 define('JPATH_BASE', __DIR__ . '/');
@@ -17,24 +17,12 @@ $query = $db->getQuery(true);
 
 // The requête
 $db->setQuery("select * from #__content where state='1'");
+
 $data = $db->loadObjectList();
 
-$i = 0;
-
-/* Ici, enr correspond à un enregistrement (une ligne) de la Base de données */
-
-foreach ( $data AS $enr )
-{
-	$tab[$i] = $enr->id;
-	$i++;	
-}
-
-// On compte les articles, et on en choisi un au hasard entre le premier et le dernier
-
-$totalArticle = count($tab);
-$idArticle = rand($tab[0],$totalArticle);
+$idArticle = array_rand($data, 1);
 
 // Here, you move towards the random article.
-header("Refresh: 0;URL=index.php?option=com_content&view=article&id=".$tab[$idArticle]);
+header("Refresh: 0;URL=index.php?option=com_content&view=article&id=".$data[$idArticle]->id);
 
 ?>
