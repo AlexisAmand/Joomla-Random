636e74841976cf8286a33aafbe54ca70c1858fb5
alexisamand <alexis.amand@gmail.com>
2020-12-28 16:52:19 +0100

    Mise à jour des commmentaires

--- random.php
+++ random.php
@@ -2,7 +2,7 @@
 
 /* Affichage d'un article aléatoire d'un blog qui tourne avec Joomla 3.X */
 /* Cette version utilise l'API de Joomla */
-/* version 0.3 */
+/* version 0.3.1 */
 
 define('_JEXEC', 1);
 define('JPATH_BASE', __DIR__ . '/');
@@ -15,14 +15,14 @@ $db = JFactory::getDbo();
 // La requête est un objet
 $query = $db->getQuery(true);
 
-// The requête
+// The requête pour avoir tout les articles 
 $db->setQuery("select * from #__content where state='1'");
-
 $data = $db->loadObjectList();
 
+// On prends un article au hasard parmi tous ceux de la liste
 $idArticle = array_rand($data, 1);
 
-// Here, you move towards the random article.
+// Ici, ça affiche l'article
 header("Refresh: 0;URL=index.php?option=com_content&view=article&id=".$data[$idArticle]->id);
 
 ?>
--- randomsansapi.php
+++ randomsansapi.php
@@ -2,13 +2,13 @@
 
 /* Affichage d'un article aléatoire d'un blog qui tourne avec Joomla 3.X */
 /* Cette version n'utilise pas l'API de Joomla */
-/* version 0.6.1 */
+/* version 0.6.2 */
 
+/* Récupération de la config de Joomla */
 require_once 'configuration.php' ;
 $config =new JConfig();
 
-/* Affichage de l'article */
-
+/* Connexion à mysql pour avoir accés à la BD de Joomla */
 try {
 	$pdo = new PDO ('mysql:host='.$config->host .';dbname='.$config->db, $config->user, $config->password);
 	// $pdo = new PDO('mysql:charset=utf8mb4');
@@ -19,6 +19,7 @@ catch ( Exception $e )
 	die ( 'Erreur: '.$e->getMessage () );
 	}
 
+// The requête pour avoir tout les articles 
 $sql = "select * from ".$config->dbprefix."content where state='1'";
 $req = $pdo->query ( $sql );
 
@@ -29,10 +30,14 @@ while ( $row = $req->fetch (PDO::FETCH_ASSOC) )
 	$tab[$i] = $row['id'];		
 	$i++;
 	}
-	
+
+/* Nombre total d'articles */
 $totalArticle = count($tab);
+
+// On prends un article au hasard parmi tous ceux de la liste
 $idArticle = rand(5,$totalArticle);
 
+// Ici, ça affiche l'article
 header("Refresh: 0;URL=index.php?option=com_content&view=article&id=".$tab[$idArticle]);
 
 ?>
